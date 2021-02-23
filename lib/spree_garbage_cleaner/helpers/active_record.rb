2f66df22a62e2ee960e699de89ff0f6c41dd3091
Asher Van Brunt <asher@vanbrunt.me>
2021-02-23 14:38:53 -0800

    Add debug output

--- lib/spree_garbage_cleaner/helpers/active_record.rb
+++ lib/spree_garbage_cleaner/helpers/active_record.rb
@@ -6,14 +6,15 @@ module SpreeGarbageCleaner
       end
 
       module ClassMethods
-        def destroy_garbage
+        def destroy_garbage(debug: false)
           destroyed = []
 
           garbage.find_each(batch_size: Spree::GarbageCleaner::Config.batch_size) do |r|
             begin
               destroyed << r.destroy
+              puts '.' if debug
             rescue StandardError => e
-              puts "Unable to destroy #{r.class} #{r&.id}"
+              puts "Unable to destroy #{r.class} #{r&.id}: #{e.message}"
             end
           end
 
--- lib/tasks/garbage.rake
+++ lib/tasks/garbage.rake
@@ -6,7 +6,7 @@ namespace :db do
       garbage_models = Spree::GarbageCleaner::Config.models_with_garbage.delete(' ').split(',').map(&:constantize)
 
       garbage_models.each do |model|
-        destroyed = model.destroy_garbage
+        destroyed = model.destroy_garbage(debug: true)
         puts "Destroyed #{destroyed.length} garbage records from #{model}\n"
       end
     end
